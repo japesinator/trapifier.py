2a100308178ef55cc65f3ebeda864b4463b3e3e4
JP Smith <jp.smith@ilstu.edu>
2014-01-24 20:10:51 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+Trapifier.py
+============
+
+This is a script for turning boring, regular sound files into CERTIFIED TRAP BANGERS by overdubbing a variety of samples.  Have you ever wished that boring old Sufjan Stevens track had about 500% more foghorn?  Consider your wish granted.
+
+Requirements
+------------
+
+This program uses python, so make sure you have that installed.  Pretty much any version should work.  It also uses [pydub](http://pydub.com/) for most of the audio processing.  `pip install pydub` works, and so does cloning the git repository.
+
+Installation
+------------
+
+Clone the repository and then run trapifier.py.
+
+Usage
+-----
+
+Run trapifier.py <inputfile> <outputfile>, where <inputfile> is song to be converted, and <outputfile> is desired title of the created track.  You can also add --samples <directory> to use samples from that directory.
+
+Acknowledgements
+----------------
+
+This program was inspired by [this reddit thread](http://www.reddit.com/r/hiphopheads/comments/1vxdag/guys_i_need_a_favor/).  The samples used are from [this person on reddit](http://www.reddit.com/r/DJs/comments/1vhaez/sample_pack_not_sure_if_anyone_is_interested_but/).
+
+License
+-------
+
+This program is MIT licensed.
+
+Copyright (c) 2014, JP Smith, japesinator.github.io
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ trapifier.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import pydub
+import random
+import argparse
+import os
+
+# Take command-line arguments for input and output files
+parser = argparse.ArgumentParser(description="Turn boring sound files into \
+                                 Real Trap Shit.")
+parser.add_argument("inputfile", metavar='inputfile',
+                    help="The input file, a mp3, wav, ogg, or flv")
+parser.add_argument("outputfile", metavar='outputfile',
+                    help="The output filename")
+parser.add_argument("--samples", nargs='?',
+                    help="The directory to look in \
+                    for samples.  Defaults to \"samples\"")
+
+
+# Get a file extension for a given file
+def extension(file):
+    ext = os.path.splitext(file)[-1].lower()
+    return ext
+
+
+# Interpret the input file based on its codec
+def parse():
+    global base_track
+    if extension(inputfile) == ".wav":
+        base_track = pydub.AudioSegment.from_wav(inputfile)
+    elif extension(inputfile) == ".mp3":
+        base_track = pydub.AudioSegment.from_mp3(inputfile)
+    elif extension(inputfile) == ".ogg":
+        base_track = pydub.AudioSegment.from_ogg(inputfile)
+    elif extension(inputfile) == ".flv":
+        base_track = pydub.AudioSegment.from_flv(inputfile)
+    else:
+        raise NameError("Please use a .wav, .mp3, .ogg, or .flv file as input")
+
+    # Check if we're using custom samples
+    global sample_dir
+    if args.samples:
+        sample_dir = args.samples
+    else:
+        sample_dir = "samples"
+
+
+# Actually put the samples over the base track
+def overlay():
+    # Define our variables
+    output = base_track
+    break_length = 0
+    not_done = False
+
+    while not_done is False:
+        # Pick out a random sample to overlay
+        overlay_sound_file = random.choice(os.listdir(sample_dir))
+        # Parse it based on its codec
+        if extension(overlay_sound_file) == ".wav":
+            overlay_sound = pydub.AudioSegment.from_wav(str(sample_dir) + '/'
+                                                        + overlay_sound_file)
+        else:
+            overlay_sound = pydub.AudioSegment.from_mp3(str(sample_dir) + '/'
+                                                        + overlay_sound_file)
+
+        # Actually put it over the track
+        output = output.overlay(overlay_sound, position=break_length)
+
+        # Pick out a length from 1.5 - 6 seconds to wait
+        break_length += random.randrange(1500, 6000)
+
+        # If we'd be waiting until the track ended, stop the loop
+        if break_length > len(base_track):
+            not_done = True
+
+    # Save the file as an MP3
+    output.export(outputfile, format="mp3")
+
+# Actually do things
+if __name__ == "__main__":
+    args = parser.parse_args()
+    inputfile = args.inputfile
+    outputfile = args.outputfile
+    parse()
+    overlay()
